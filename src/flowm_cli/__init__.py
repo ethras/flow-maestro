e34b6107ebcac1447c004adfc03531ed08f03d09
Ethras <vladimirmbassi@gmail.com>
2025-10-03 17:25:08 +0200

    chore(release): bump version to 0.1.1

--- src/flowm_cli/__init__.py
+++ src/flowm_cli/__init__.py
@@ -39,7 +39,7 @@ from .core import (
 )
 
 APP_NAME = "flowm"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 OWNER = os.getenv("FLOWM_REPO_OWNER", "ethras")
 REPO = os.getenv("FLOWM_REPO_NAME", "flow-maestro")
 
