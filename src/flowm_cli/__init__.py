2bccf1fba8d34d4975c841ad348b5196c55a7285
Ethras <vladimirmbassi@gmail.com>
2025-10-06 10:42:20 +0200

    chore(release): v0.2.5

--- src/flowm_cli/__init__.py
+++ src/flowm_cli/__init__.py
@@ -39,7 +39,7 @@ from .core import (
 )
 
 APP_NAME = "flowm"
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 OWNER = os.getenv("FLOWM_REPO_OWNER", "ethras")
 REPO = os.getenv("FLOWM_REPO_NAME", "flow-maestro")
 
